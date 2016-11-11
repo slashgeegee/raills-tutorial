7406aefda810f868325cb21b918d6a236b10a2e5
kboygit <heybisayangdako@gmail.com>
2016-11-11 11:21:57 -0800

    Push to heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello word!"
+    render html: "Welcome to Rapollo"
   end
 end
